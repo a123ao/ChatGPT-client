5ed2b5a7aca083fe92b336b2527b609221152156
lukas <lukas7865@yahoo.co.uk>
2025-05-21 00:28:32 +0100

    Add models

--- src/core/types/model.ts
+++ src/core/types/model.ts
@@ -1,5 +1,7 @@
 export enum ModelType {
+    GPT41Mini   = 'gpt-4-1-mini',
     GPT4oMini   = 'gpt-4o-mini',
     GPT4o       = 'gpt-4o',
+    O4Mini      = 'o4-mini',
     O3Mini      = 'o3-mini'
 }
